c89fa7da0c7ce4b202d668ed72e79c758494c8a5
Mischa <mischakv>
2023-02-25 21:34:52 +0100

    tested normalization of pdf, plots now z-Axis for checking if moves to maximum

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from MPC_control_pdf import mpc_control
 from stuff import read_prob_map, get_pdf
 from scipy.interpolate import LinearNDInterpolator
 from tqdm import tqdm
+from sklearn import preprocessing
 
 # choose between two different maps
 map_nr = 2
@@ -23,7 +24,7 @@ elif map_nr == 2:
 count = 0
 
 # initial values for mpc
-steps = 500  # steps to be calculated
+steps = 300  # steps to be calculated
 dt = 1/5  # time per steps
 T = 15  # horizon
 
@@ -45,14 +46,16 @@ _x, _y = np.mgrid[0:(boxes_number_width-1), 0:(boxes_number_height-1)]
 xy = np.column_stack([_x.flat, _y.flat])
 zg = get_pdf(n_components, xy, means, covariances)
 
-# interpolate discrete pdf
-interp_func = LinearNDInterpolator(list(zip(xy[:,0],xy[:,1])), zg)
-
 # reshape discrete pdf
 zg = zg.reshape(_x.shape)
+#zg = preprocessing.normalize(zg)
+
+# interpolate discrete pdf
+interp_func = LinearNDInterpolator(list(zip(xy[:,0],xy[:,1])), zg.flatten())
 
 # init empty arrays
 t = np.linspace(0, steps*dt, steps)
+ol_t = np.linspace(0, T*dt, T)
 
 # Closed loop
 x = np.zeros(steps)
@@ -112,14 +115,12 @@ ax3.scatter(init_x,init_y, color="green", label="Start")
 ax3.plot(ol_x, ol_y, linewidth=3, color='r',alpha=1)
 ax3.set_title('Open Loop')
 
-ax4 = fig.add_subplot(1, 2, 2, projection = '3d')
-ax4.plot_surface(Xnew,Ynew,interp_func(Xnew, Ynew), cmap=cm.jet, alpha=0.6)
-ax4.scatter(init_x,init_y, interp_func(init_x,init_y), color="green", label="Start")
-ax4.plot(x, y, z, linewidth=3, color='r',alpha=1)
-ax4.set_title('Closed Loop')
+ax5 = fig.add_subplot(1, 2, 2)
+ax5.plot(ol_t, interp_func(ol_x,ol_y), linewidth=3, color='r',alpha=1)
+ax5.set_title('Z-Axis Open Loop')
 
 fig = plt.figure()
-ax1 = fig.add_subplot(1, 2, 1)
+ax1 = fig.add_subplot(1, 3, 1)
 ax1.plot(t, u_x, label="Acceleration x")
 ax1.plot(t, u_y, label='Acceleration y')
 ax1.set_xlabel("Time")
@@ -127,11 +128,22 @@ ax1.set_ylabel("Acceleration")
 ax1.legend()
 ax1.set_title('Inputs')
 
-ax2 = fig.add_subplot(1, 2, 2)
+ax2 = fig.add_subplot(1, 3, 2)
 ax2.plot(x, y)
 ax2.scatter(init_x,init_y, color="green", label="Start")
 ax2.scatter(means[:,0], means[:,1],means[:,2]*100, color='r', alpha=1)
 ax2.grid(True)
 ax2.set_title('Drone trajectory')
+
+ax5 = fig.add_subplot(1, 3, 3)
+ax5.plot(t, interp_func(x,y), linewidth=3, color='r',alpha=1)
+ax5.set_title('Z-Axis Closed Loop')
+
+fig = plt.figure()
+ax4 = fig.add_subplot(1, 1, 1, projection = '3d')
+ax4.plot_surface(Xnew,Ynew,interp_func(Xnew, Ynew), cmap=cm.jet, alpha=0.6)
+ax4.scatter(init_x,init_y, interp_func(init_x,init_y), color="green", label="Start")
+ax4.plot(x, y, z, linewidth=3, color='r',alpha=1)
+ax4.set_title('Closed Loop')
 
 plt.show()
