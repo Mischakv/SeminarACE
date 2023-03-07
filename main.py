c974c5fd986aadcb2783e127902cd9b515d62aa7
Mischa <mischakv>
2023-03-07 18:57:58 +0100

    added costfunction plot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import matplotlib
 import matplotlib.pyplot as plt
 from sklearn.mixture import GaussianMixture
 from matplotlib import cm
@@ -11,6 +12,8 @@ from sklearn import preprocessing
 from sklearn.model_selection import GridSearchCV
 import pandas as pd
 from sklearn import metrics
+import time
+matplotlib.rcParams.update({'font.size': 17})
 
 # choose between two different maps
 map_nr = 2
@@ -29,7 +32,7 @@ count = 0
 # initial values for mpc
 steps = 400  # steps to be calculated
 dt = 1/5  # time per steps
-T = 30  # horizon
+T = 45  # horizon
 
 # inital x and y values
 init_x = 7
@@ -122,18 +125,24 @@ ol_x[0] = init_x
 ol_y[0] = init_y
 z[0] = interp_func(init_x,init_y)
 
+start_time = time.time()
 # calculate the optimal input
-inputs = mpc_control(init_x,init_y,interp_func,0,0,T,10*np.identity(1),10*np.identity(1),dt)
+inputs = mpc_control(init_x,init_y,interp_func,0,0,T,1000*np.identity(1),np.identity(1),dt)
+
 # first open loop inputs
 first_ol_input = inputs
-
+cost = 0
 # calculate x and y for open loop input
 for i in range(T-1):
     new_ol_x += first_ol_input[0][i]*dt**2
     new_ol_y += first_ol_input[1][i]*dt**2
     ol_x[i+1] = new_ol_x
     ol_y[i+1] = new_ol_y
-    ol_z[i+1] = interp_func(new_ol_x,new_ol_y)
+    cost += -interp_func(new_ol_x,new_ol_y)*1000
+    #print(np.dot(first_ol_input[0][i], first_ol_input[0][i]))
+    cost += np.dot(first_ol_input[0][i], first_ol_input[0][i])
+    cost += np.dot(first_ol_input[1][i], first_ol_input[1][i])
+    ol_z[i+1] = cost#interp_func(new_ol_x,new_ol_y)
 
 # closed loop for defined steps
 for i in tqdm(range(steps-1), desc ="Progress: "):
@@ -145,24 +154,43 @@ for i in tqdm(range(steps-1), desc ="Progress: "):
     u_x[i+1] = inputs[0][0]
     u_y[i+1] = inputs[1][0]
     inputs = mpc_control(new_x,new_y,interp_func,inputs[0][0],inputs[1][0],T,1000*np.identity(1),np.identity(1),dt)
-
+print("--- %s seconds ---" % (time.time() - start_time))
 # plot solution
 xnew = np.linspace(0, boxes_number_width-1, 800)
 ynew = np.linspace(0, boxes_number_height-1, 800)
 Xnew, Ynew = np.meshgrid(xnew, ynew)
 
 fig = plt.figure()
-ax3 = fig.add_subplot(1, 2, 1)
-ax3.scatter(init_x,init_y, color="green", label="Start")
-ax3.plot(ol_x, ol_y, linewidth=3, color='r',alpha=1)
-ax3.set_title('Open Loop')
+ax9 = fig.add_subplot(1, 4, 1)
+ax9.plot(ol_t, inputs[0], label="Acceleration x")
+ax9.plot(ol_t, inputs[1], label='Acceleration y')
+ax9.set_xlabel("Time")
+ax9.set_ylabel("Acceleration")
+ax9.legend()
+ax9.set_title('Inputs')
 
-ax5 = fig.add_subplot(1, 2, 2)
-ax5.plot(ol_t, interp_func(ol_x,ol_y), linewidth=3, color='r',alpha=1)
-ax5.set_title('Z-Axis Open Loop')
+ax3 = fig.add_subplot(1, 4, 2)
+ax3.plot(ol_x, ol_y, linewidth=1, color='r',alpha=1, label="path")
+ax3.scatter(init_x,init_y, color="green", label="Start")
+ax3.set_xlabel("x-direction")
+ax3.set_ylabel("y-direction")
+ax3.legend()
+ax3.set_title('Map')
+
+ax8 = fig.add_subplot(1, 4, 3)
+ax8.plot(ol_t, interp_func(ol_x,ol_y)*1000, linewidth=1, color='r',alpha=1)
+ax8.set_xlabel("Time")
+ax8.set_ylabel("Probability")
+ax8.set_title('Probability on map')
+
+ax5 = fig.add_subplot(1, 4, 4)
+ax5.plot(ol_t, ol_z, linewidth=1, color='r',alpha=1)
+ax5.set_xlabel("Time")
+ax5.set_ylabel("Value")
+ax5.set_title('Value of costfunction')
 
 fig = plt.figure()
-ax1 = fig.add_subplot(1, 3, 1)
+ax1 = fig.add_subplot(1, 4, 1)
 ax1.plot(t, u_x, label="Acceleration x")
 ax1.plot(t, u_y, label='Acceleration y')
 ax1.set_xlabel("Time")
@@ -170,7 +198,7 @@ ax1.set_ylabel("Acceleration")
 ax1.legend()
 ax1.set_title('Inputs')
 
-ax2 = fig.add_subplot(1, 3, 2)
+ax2 = fig.add_subplot(1, 4, 2)
 ax2.plot(x, y, label="Path")
 ax2.scatter(init_x,init_y, color="green", label="Start")
 ax2.scatter(means[:,0], means[:,1],means[:,2]*100, color='r', alpha=1, label="GMM Means")
@@ -180,11 +208,20 @@ ax2.set_ylabel("Y-direction")
 ax2.legend()
 ax2.set_title('Drone trajectory')
 
-ax5 = fig.add_subplot(1, 3, 3)
-ax5.plot(t, interp_func(x,y), linewidth=3, color='r',alpha=1)
+ax5 = fig.add_subplot(1, 4, 3)
+ax5.plot(t, interp_func(x,y)*1000, linewidth=3, color='r',alpha=1)
 ax5.set_xlabel("Time")
 ax5.set_ylabel("Probability")
 ax5.set_title('Probability on Map - Closed Loop')
+
+ax4 = fig.add_subplot(1, 4, 4, projection = '3d')
+ax4.plot_surface(Xnew,Ynew,interp_func(Xnew, Ynew), cmap=cm.jet, alpha=0.6)
+ax4.scatter(init_x,init_y, interp_func(init_x,init_y), color="green", label="Start")
+ax4.plot(x, y, z, linewidth=3, color='r',alpha=1)
+ax4.set_title('Probability Map')
+ax4.view_init(elev=58., azim=-45)
+ax4.set_xlabel("X-direction")
+ax4.set_ylabel("Y-direction")
 
 fig = plt.figure()
 ax4 = fig.add_subplot(1, 1, 1, projection = '3d')
